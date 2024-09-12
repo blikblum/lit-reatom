f793d0d102e3220849369109bc4502ebbc1d0ddb
Luiz Américo Pereira Câmara <blikblum@users.noreply.github.com>
2024-09-12 18:02:37 -0300

    v0.2.0

--- types/withStore.d.ts
+++ types/withStore.d.ts
@@ -1,18 +1,16 @@
 /**
- * @returns {import('@reatom/core').Ctx}
+ * @returns {Ctx | undefined}
  */
-export function getDefaultCtx(): import('@reatom/core').Ctx;
+export function getDefaultCtx(): Ctx | undefined;
 /**
- * @param {import('@reatom/core').Ctx} ctx
+ * @param {Ctx} ctx
  */
-export function setDefaultCtx(ctx: import('@reatom/core').Ctx): void;
+export function setDefaultCtx(ctx: Ctx): void;
 /**
  * @template {typeof HTMLElement} BaseClass
  * @param {BaseClass} BaseClass
  * @returns {BaseClass}
  */
-export function withStore<BaseClass extends {
-    new (): HTMLElement;
-    prototype: HTMLElement;
-}>(BaseClass: BaseClass): BaseClass;
+export function withStore<BaseClass extends typeof HTMLElement>(BaseClass: BaseClass): BaseClass;
+import type { Ctx } from '@reatom/core';
 //# sourceMappingURL=withStore.d.ts.map
